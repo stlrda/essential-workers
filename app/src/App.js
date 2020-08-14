b2a1bd03834a166f10d8e857ffd68e2c6271c957
Moreland <TIM0416@daugherty.com>
2020-08-14 02:31:52 -0500

    Updated placeholder text

--- app/src/App.js
+++ app/src/App.js
@@ -20,9 +20,12 @@ function App() {
       <Hero />
       <article>
         <p id="summary">
-        Accumsan tortor posuere ac ut consequat semper viverra. Arcu non odio euismod lacinia at quis. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Tellus in hac habitasse platea dictumst vestibulum rhoncus. Viverra aliquet eget sit amet. Sed viverra ipsum nunc aliquet bibendum. Aliquet bibendum enim facilisis gravida neque convallis a cras. Id semper risus in hendrerit gravida rutrum. Enim ut sem viverra aliquet. Aliquam purus sit amet luctus venenatis. Velit ut tortor pretium viverra. Diam donec adipiscing tristique risus. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Eget sit amet tellus cras adipiscing enim eu turpis egestas. Tristique risus nec feugiat in. Id faucibus nisl tincidunt eget nullam non nisi. Felis imperdiet proin fermentum leo vel orci. Tempus imperdiet nulla malesuada pellentesque elit eget gravida. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Tortor pretium viverra suspendisse potenti nullam.
+          Accumsan tortor posuere ac ut consequat semper viverra. Arcu non odio euismod lacinia at quis. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Tellus in hac habitasse platea dictumst vestibulum rhoncus. Viverra aliquet eget sit amet. Sed viverra ipsum nunc aliquet bibendum. Aliquet bibendum enim facilisis gravida neque convallis a cras. Id semper risus in hendrerit gravida rutrum. Enim ut sem viverra aliquet. Aliquam purus sit amet luctus venenatis. Velit ut tortor pretium viverra. Diam donec adipiscing tristique risus. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Eget sit amet tellus cras adipiscing enim eu turpis egestas. Tristique risus nec feugiat in. Id faucibus nisl tincidunt eget nullam non nisi. Felis imperdiet proin fermentum leo vel orci.
         </p>
         <Map />
+        <p id="table-summary">
+          Magna ac placerat vestibulum lectus mauris ultrices eros. Urna condimentum mattis pellentesque id. Sed viverra ipsum nunc aliquet bibendum. Purus in massa tempor nec. Vitae nunc sed velit dignissim sodales ut eu sem integer. Ullamcorper sit amet risus nullam. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Dolor sit amet consectetur adipiscing elit ut aliquam. Ridiculus mus mauris vitae ultricies leo integer malesuada nunc. Neque vitae tempus quam pellentesque nec nam aliquam.
+        </p>
         <Table />
         <Footer />
       </article>
