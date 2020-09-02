13d6a1e9a04a965076b2f8667a7a10a0f8a88786
Moreland <TIM0416@daugherty.com>
2020-09-02 17:13:52 -0500

    Finsished text for Footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -15,9 +15,11 @@ const Footer = (props) => {
         The Essential Workers project was developed by 
         the <a target="_blank" rel="noopener noreferrer" href="http://stldata.org">St. Louis Regional Data Alliance </a>
         in partnership with <a target="_blank" rel="noopener noreferrer" href="http://daugherty.com">Daugherty Business Solutions</a>.  
-        The data used in this project was collected from the Census Bureau's American Community Survey (2018)
-        and the Bureau of Economic Analysis. Each dataset featured on this webpage can be downloaded via the upper right-hand corner of the
-        webpage.
+        The data used in this project was collected from the Census Bureau 
+        (<a target="_blank" rel="noopener noreferrer" href="https://data.census.gov/mdat/#/search?ds=ACSPUMS5Y2018">Amercian Community Survey 5-year Estimates Public Use Microdata Sample 2018</a>)
+        and the Bureau of Economic Analysis 
+        (<a target="_blank" rel="noopener noreferrer" href="https://apps.bea.gov/iTable/iTable.cfm?reqid=99&step=1#reqid=99&step=1">GDP and Personal Income 2018</a>). 
+        All of the data collected can be downloaded by clicking on the "Download Data" button to the right of the navigation bar.
         
         </Grid>
 
