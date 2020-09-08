d79914fece58c9b793d7d3d2dd57120946b1ec7e
Moreland <TIM0416@daugherty.com>
2020-09-08 18:12:42 -0500

    Text content now updated to include how industries were grouped

--- app/src/App.js
+++ app/src/App.js
@@ -76,13 +76,14 @@ function App() {
               <StaticControlPanel tableNames={Object.keys(tables)} currentView={selectedTableName} setTableView={setSelectedTableName}/>
           </Hidden>
 
+          <Typography variant="body1" id="intro">
+            Essential workers are shouldering the responsibility of providing fundamental products and services 
+            during the COVID-19 pandemic all while being at especially high risk for exposure to the virus.
+            This webpage is dedicated to helping others garner insight into the economic climate surrounding
+            more than 2,000,000 essential workers across Missouri and Illinois.
+          </Typography>
+
           <Typography variant="body1" id="map-summary">
-            During the pandemic, essential workers are shouldering the burden of providing
-            necessary products and services all while being at especially high risk for exposure to the virus.
-            The visualizations below illustrate demographics and profiles of essential workers prior to the
-            pandemic across Missouri, Illinois, and Saint Louis.
-            <br/>
-            <br/>
             The interactive map below features county level data on essential workers across five measures. 
             You are currently viewing data for {selectedTableName}.
             In {selectedTableName}, {map_summary[selectedTableName]}
@@ -92,10 +93,58 @@ function App() {
           <Map table={selectedTableName} />
  
           
+          <Typography variant="body1" id="segway">
+            The data table below details characteristics of essential workers by overall quantity and percentage, generalized into
+            six groups. In generalizing frontline / essential industries, we took inspiration from the
+            <a href="https://cepr.net/a-basic-demographic-profile-of-workers-in-frontline-industries/"> Center for Economic and Policy Research</a>.
+            Essential industry groups feature various, more specific industries, each classified by the Census Bureau’s Industry Codes:
+          </Typography>
+
+          <Typography variant="body1" id="groupings">
+            <ul>
+                <li>
+                  Grocery, Convenience, and Drug Stores: Grocery and related product merchant wholesalers (4470)
+                  , Supermarkets and other grocery stores (4971)
+                  , Convenience Stores (4972)
+                  , Pharmacies and drug stores (5070)
+                  , and General merchandise stores
+                  , including warehouse clubs and supercenters (5391)
+                </li>
+                <li>
+                  Public Transit: Rail transportation (6080) and Bus service and urban transit (6180).
+                </li>
+                <li>
+                  Trucking, Warehouse
+                  , and Postal Service: Truck transportation (6170)
+                  , Warehousing and storage (6390)
+                  , and Postal Service (6370).
+                </li>
+                <li>
+                  Building Cleaning Services: Cleaning Services to Buildings and Dwellings (7690).
+                </li>
+                <li>
+                  Health Care: Offices of physicians (7970)
+                  , Outpatient care centers (8090)
+                  , Home health care services (8170)
+                  , Other health care services (8180)
+                  , General medical and surgical hospitals
+                  , and specialty hospitals (8191)
+                  , Psychiatric and substance abuse hospitals (8192)
+                  , Nursing care facilities (skilled nursing facilities) (8270)
+                  , and Residential care facilities
+                  , except skilled nursing facilities (8290).
+                </li>
+                <li>
+                  Child Care and Social Services: Individual and family services (8370)
+                  , Community food and housing
+                  , and emergency services (8380)
+                  , and Child day care services (8470).
+                </li>
+              </ul>
+          </Typography>
 
           <Typography variant="body1" id="table-summary">
-            The data table below details characteristics of essential workers by overall quantity and percentage, broken up into
-            six generalized industries. You are currently viewing data for {selectedTableName}.
+            You are currently viewing data for {selectedTableName}.
           </Typography>
 
           
